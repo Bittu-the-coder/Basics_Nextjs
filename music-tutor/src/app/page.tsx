ffd4088d9ab051afcb730e8a2edef60334737cfd
Bittu Prajapati <Bittuprajapati2271@gmail.com>
2025-02-13 01:58:28 +0530

    Add files via upload

---
+++ music-tutor/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-gradient-to-t from-black via-black to-gray-900">
+        <h1 className="text-2xl text-center">Musicia</h1>
+      </main>
+    </>
+  );
+}
