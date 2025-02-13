c926cc1643da5bf5346b96868192ca0d7179a2b2
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-02-13 19:01:53 +0530

    removed

--- music-tutor/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <main className="min-h-screen bg-black/[0.96] antialiased bg-gradient-to-t from-black via-black to-gray-900">
-        <h1 className="text-2xl text-center">Musicia</h1>
-      </main>
-    </>
-  );
-}
